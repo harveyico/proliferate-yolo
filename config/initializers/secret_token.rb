95a64706d5a9d097cf56fd0870e7fcbfc9f01086
Adrian Peterson Co <adrianpco@gmail.com>
2013-04-08 11:50:12 +0800

    Starts something new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InternMe::Application.config.secret_token = 'eaf959759804284eb48d27e2dc37c84671c04274d63c0cdc4b126aa3f362e512f915086afcf47d02b8f9d57215868abd25a3b58f2c70fd5a502d169eb8a913a1'
